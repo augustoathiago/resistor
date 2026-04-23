d7877b8889fc2262c885696046555b58ca7f066d
augustoathiago <augusto.a.thiago@gmail.com>
2026-04-22 21:28:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import math
 # Constantes fixas do simulador
 # ---------------------------
 V_MAX = 30.0           # tensão máxima (V) e limite superior do eixo Y
-R_MIN = 750.0          # resistência mínima (Ω)
+R_MIN = 300.0          # resistência mínima (Ω)
 R_MAX = 10000.0        # resistência máxima (Ω)
 X_MAX_mA = 40.0        # limite do eixo X (mA)
 
